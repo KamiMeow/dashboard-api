e49265ff605fd85b4fded7cad263a090dc4e04c6
Kami San <1.dankon.1@gmail.com>
2019-10-11 00:51:59 +0300

    Config folder

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 const config = {};
 
-config.PORT = process.env.PORT || 5050;
+config.PORT = process.env.PORT || 5000;
 
 config.DB = {
   sertificate: 'mongodb',
   baseUrl: 'localhost',
   dbName: 'dashboard',
-  port: '27012',
+  port: '27017',
 };
 
 config.CONNECTION_STRING = `${config.DB.sertificate}://${config.DB.baseUrl}:${config.DB.port}/${config.DB.dbName}`;
--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
-const CONNECTION_STRING = require('./config').CONNECTION_STRING;
 
 module.exports.connect = (callback) => {
-  mongoose.connect(CONNECTION_STRING, {
+  mongoose.connect(config.CONNECTION_STRING, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   }, callback);
