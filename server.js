dfe041b1e087ce6fed2d8749dc9a99d9d57fd2b8
Kami San <1.dankon.1@gmail.com>
2019-10-11 00:52:30 +0300

    File with server config

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require('./app');
 
-app.listen(global.config.PORT, (err) => {
+app.init();
+app.listen(config.PORT, (err) => {
   if (err) throw err;
-
-  app.connectToDB()
-
-  console.log('Start server on port ' + global.config.PORT);
+  app.connectToDB();
 });
