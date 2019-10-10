e70b48b79d6277817b920f4e0d0200bde35ad81f
Kami San <1.dankon.1@gmail.com>
2019-10-11 01:12:33 +0300

    Router InfoType

---
+++ app/InfoType/index.js
@@ -0,0 +1,47 @@
+const InfoType = require('InfoType');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  InfoType.find({}, (err, types) => {
+    if (err) throw err;
+    res.send(types);
+  });
+});
+router.get('/:id', (req, res) => {
+  InfoType.findById({ _id: req.params.id }, (err, type => {
+    if (err) throw err;
+    res.end(type);
+  }));
+});
+
+router.post('/', (req, res) => {
+  const { name, type, validation } = req.body
+  InfoType.create({ name, type, validation }, (err, type) => {
+    res.status(201).send(type);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  InfoType.findById(req.params.id, (err, infoType) => {
+    const name = req.body.name || infoType.name;
+    const type = req.body.type || infoType.type;
+    const validation = req.body.validation || infoType.validation;
+
+    InfoType.updateOne({ _id: infoType._id },
+      { name, type, validation },
+      { new: true },
+      (err, type) => {
+        if (err) throw err;
+        res.send(type);
+      });
+  });
+});
+
+router.delete(':/id', (req, res) => {
+  InfoType.findById(req.params.id, (err, type) => {
+    if (err) throw err;
+    res.send(type);
+  });
+});
+
+module.exports = router;
