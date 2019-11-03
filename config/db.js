eab2f54bc4f44dedc84ea0dd41bee1effaa577c9
Kami San <1.dankon.1@gmail.com>
2019-11-03 11:48:09 +0300

    deprecated old functions

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 module.exports.connect = (callback) => {
   mongoose.connect(config.CONNECTION_STRING, {
     useUnifiedTopology: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
+    useCreateIndex: true,
   }, callback);
 };
