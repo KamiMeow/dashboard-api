bb69f61915bc5b867624b45e956f7021d4e5c4cd
Kami San <1.dankon.1@gmail.com>
2019-11-15 18:23:20 +0300

    create statistic router

---
+++ app/Statistic/index.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const Repos = require('./Repos');
+const Statistic = require('./Statistic');
+const User = require('../User/User');
+const auth = require('../router/auth');
+
+async function currentDataFromAPI(account) {
+  const data = await Repos[account.name](account.value);
+  data.origin = account.name;
+  return data;
+};
+
+router.get('/refresh', auth.required, async (req, res) => {
+  const { id } = req.payload;
+  const user = await User.findById(id);
+
+  user.getProfile(async (err, { accounts }) => {
+    data = accounts.map(account => currentDataFromAPI(account));
+    await Promise.all(data)
+      .then(data => {
+        console.log('data', data);
+        res.status(200).send(data);
+      })
+  })
+});
+
+router.get('/', auth.required, async (req, res) => {
+  const { id } = req.payload;
+  const currentStatistics = await Statistic.find({
+    user: id,
+  });
+  res.status(200).send(currentStatistics);
+});
+
+module.exports = router;
--- app/router/index.js
+++ app/router/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 router.use('/statistic-types', require('../StatisticType'));
 router.use('/info-types', require('../InfoType'));
+router.use('/statistic', require('../Statistic'))
 router.use('/accounts', require('../Accounts'));
-router.use('/github', require('../GitHub'));
 router.use('/user', require('../User').userRouter);
 
 module.exports = router;
