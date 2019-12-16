1d1570347b23b50588eee54d4dc7c0003d2b3fc6
Kami San <1.dankon.1@gmail.com>
2019-12-17 00:38:09 +0300

    Change model

--- app/Statistic/Statistic.js
+++ app/Statistic/Statistic.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const StatisticSchema = new mongoose.Schema({
   type: { type: mongoose.Schema.ObjectId, ref: 'StatisticType', required: true },
   user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  title: { type: String, unique: true, required: true },
+  title: { type: String, required: true },
   value: Object,
 });
 
-mongoose.model('Statistic', StatisticSchema);
-module.exports = mongoose.model('Statistic');
+mongoose.model('NewStatistic', StatisticSchema);
+module.exports = mongoose.model('NewStatistic');
