e1a76021a886eb2989c4dca046680b849465f964
Kami San <1.dankon.1@gmail.com>
2020-06-27 10:55:42 +0300

    add tests

---
+++ __test__/server.spec.js
@@ -0,0 +1,5 @@
+describe('server', () => {
+  it('server should work', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import http from 'http';
 
 const httpServer = http.createServer();
-// console.log(process.env.NODE_ENV);
-// console.log(process.env.PORT);
+console.log(process.env.NODE_ENV);
+console.log(process.env.PORT);
 
 httpServer.listen(5000, () => {
   console.log('Люди, люди, хуи на блюди');
