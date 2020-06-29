787b112a868113df9ef258a264296375393508f4
Kami San <1.dankon.1@gmail.com>
2020-06-29 13:51:00 +0300

    app

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+import https from 'https';
 import http from 'http';
+import app from './app';
 import './config';
 
-const httpServer = http.createServer();
+function createServer(protocol, credentials, port = 5000) {
+  const server = protocol.createServer(credentials);
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log('Люди, люди, хуи на блюди');
+  });
+}
 
-httpServer.listen(global.config.HTTP_PORT, () => {
-  console.log('Люди, люди, хуи на блюди');
-});
+createServer(https, app, global.config.HTTPS_PORT);
+createServer(http, app, global.config.HTTP_PORT);
